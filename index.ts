f089cc64f19cfefbb3703b6f7c38126440f39710
Jim Hessin <jhessin@gmail.com>
2021-05-19 21:13:39 -0600

    finished introductory exercise

--- index.ts
+++ index.ts
@@ -1,8 +1,42 @@
-import http from "http";
+/** @format */
 
+import http from 'http';
+import moment from 'moment';
+
+const greeting = 'Zenva';
 function serverCallback(req: http.RequestOptions, res: http.ServerResponse) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("");
+  const now = moment();
+  //const open = moment().hour(10).minute(0).second(0);
+  const open = moment('10', 'HH');
+  //const close = moment().hour(12).minute(0).second(0);
+  const close = moment('12', 'HH');
+  console.log(now.format());
+  console.log(`
+    now: ${now.format()}
+    open: ${open.format()}
+    close: ${close.format()}
+    `);
+  let msg = '';
+  if (now.isBetween(open, close)) {
+    msg = 'We are now open!';
+  } else if (now.isBefore(open)) {
+    msg = `Our business hours are from ${open.format(
+      'HH:mm',
+    )} to ${close.format('HH:mm')}
+    come back ${now.to(open)}.`;
+  } else {
+    msg = `Our business hours are from ${open.format(
+      'HH:mm',
+    )} to ${close.format('HH:mm')}
+    come back tomorrow.`;
+  }
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`
+    Hello ${greeting}!
+    Welcome to our page.
+    Now, it is ${now.format('h:mm a')}
+    ${msg}
+    `);
 }
 
 http.createServer(serverCallback).listen(8080);
