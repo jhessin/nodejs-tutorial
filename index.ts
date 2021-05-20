2265da3b100b61e069401d766699e72d10d1704e
Jim Hessin <jhessin@gmail.com>
2021-05-19 18:26:57 -0600

    Setup typescript.

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import http from "http";
+
+function serverCallback(req: http.RequestOptions, res: http.ServerResponse) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("");
+}
+
+http.createServer(serverCallback).listen(8080);
